7161b4993916dd0ae157f28ebe9c8765fbf12fe2
tomdieu <ivantomdio@gmail.com>
2024-08-31 00:00:17 +0100

    Refactor navigation and layout components

--- app/(app)/(tabs)/index.tsx
+++ app/(app)/(tabs)/index.tsx
@@ -3,45 +3,58 @@ import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useAuth } from "@/store/user";
 import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "expo-router";
+import { DrawerActions } from "@react-navigation/native";
 
 type Props = {};
 
 const Index = (props: Props) => {
   const { user } = useAuth();
-  
+  const navigation = useNavigation();
+
+  const openDrawer = () => {
+    navigation.dispatch(DrawerActions.openDrawer);
+  };
+
   return (
     <SafeAreaView className="flex-1 bg-slate-50">
       <ScrollView className="flex-1 p-2 py-0 ">
         <View className="min-h-[85vh]">
-
-        {/* Header */}
-        <View className="shadow-md flex-row items-center justify-between px-2 py-5 bg-white">
-          <View className="flex-row items-end gap-2">
-            <Text className="text-3xl font-inter-semibold text-gray-800">Welcome,</Text>
-            <Text className="text-2xl font-inter-regular text-gray-600">{user?.username}</Text>
-          </View>
-          {/* <TouchableOpacity>
+          {/* Header */}
+          <View className="shadow-md flex-row items-center justify-between px-2 py-5 bg-white">
+            <View className="flex-row items-end gap-2">
+              <Text className="text-3xl font-inter-semibold text-gray-800">
+                Welcome,
+              </Text>
+              <Text className="text-2xl font-inter-regular text-gray-600">
+                {user?.username}
+              </Text>
+            </View>
+            {/* <TouchableOpacity>
             <Ionicons name="settings-outline" size={24} color="gray" />
           </TouchableOpacity> */}
-        </View>
+          </View>
 
-        {/* Content */}
-        <View className="mt-6 flex-1">
-          <Text className="text-4xl font-inter-bold text-gray-900 px-2 uppercase">Ideogramme</Text>
-          <Text className="text-lg text-gray-600 mt-2 px-2">
-            Track and manage your ideograms with ease.
-          </Text>
-        </View>
+          {/* Content */}
+          <View className="mt-6 flex-1">
+            <Text className="text-4xl font-inter-bold text-gray-900 px-2 uppercase">
+              Ideogramme
+            </Text>
+            <Text className="text-lg text-gray-600 mt-2 px-2">
+              Track and manage your ideograms with ease.
+            </Text>
+          </View>
 
-        {/* Data Collection Button */}
-        <View className="mt-10 px-2">
-          <TouchableOpacity className="bg-blue-500 rounded-md py-4 flex-row items-center justify-center shadow-lg">
-            <Ionicons name="add-circle-outline" size={24} color="white" />
-            <Text className="text-lg font-inter-semibold text-white ml-2">Start New Collection</Text>
-          </TouchableOpacity>
+          {/* Data Collection Button */}
+          <View className="mt-10 px-2">
+            <TouchableOpacity className="bg-blue-500 rounded-md py-4 flex-row items-center justify-center shadow-lg">
+              <Ionicons name="add-circle-outline" size={24} color="white" />
+              <Text className="text-lg font-inter-semibold text-white ml-2">
+                Start New Collection
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        </View>
-
       </ScrollView>
     </SafeAreaView>
   );
--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,15 +1,23 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable } from "react-native";
 import React from "react";
 import { useAuth } from "@/store/user";
 import { Redirect, Stack } from "expo-router";
 import { Drawer } from "expo-router/drawer";
 import { Home } from "lucide-react-native";
+import { Image } from "react-native";
+import {
+  DrawerContentScrollView,
+  DrawerItemList,
+} from "@react-navigation/drawer";
+import { images } from "@/constants";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const AppLayout = () => {
   const { user } = useAuth();
   if (!user) {
     return <Redirect href={"/(auth)/login"} />;
   }
+  const { bottom } = useSafeAreaInsets();
   return (
     <Drawer
       screenOptions={{
@@ -18,6 +26,22 @@ const AppLayout = () => {
           marginLeft: -20,
         },
       }}
+      drawerContent={({ ...props }) => (
+        <View className="flex-1">
+          <Image
+            className="w-full h-[250px]"
+            resizeMode="contain"
+            source={images.LogoImage}
+          />
+          <DrawerContentScrollView {...props}>
+            <DrawerItemList {...props} />
+          </DrawerContentScrollView>
+
+          <Pressable style={{ bottom }}>
+            <Text>Logout</Text>
+          </Pressable>
+        </View>
+      )}
     >
       <Drawer.Screen
         name="(tabs)"
