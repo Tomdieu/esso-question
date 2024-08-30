87fbd54e1c5b20f78f31ccb35f61abb2b4e27d53
tomdieu <ivantomdio@gmail.com>
2024-08-30 12:52:35 +0100

    Refactor login screen UI and add password visibility toggle

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -16,6 +16,7 @@ import { Link } from "expo-router";
 import { Controller, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { loginSchema, LoginSchemaType } from "@/schema/login.schema";
+import { Eye, EyeOff } from "lucide-react-native";
 
 type Props = {};
 
@@ -28,32 +29,35 @@ const LoginScreen = (props: Props) => {
     resolver: zodResolver(loginSchema),
   });
 
+  const [showPassword, setShowPassword] = useState(false);
+
   const onSubmit = (data: LoginSchemaType) => {
     console.log(data);
   };
 
   return (
     <SafeAreaView className="flex h-full jus)tify-between bg-white ">
-      <ScrollView className="flex-1 bg-white">
+      <ScrollView bounces className="flex-1 bg-white">
         <TouchableWithoutFeedback
           onPress={Keyboard.dismiss}
-          className="flex-1 min-h-[95vh] justify-center items-center h-full"
+          className="flex-1  justify-center items-center h-full w-full"
         >
-          <View className="flex-1 bg-white">
-            <View className="relative w-full h-[250px]">
+          <View className="flex-1 min-h-[100vh] bg-white">
+            <View className="relative w-full h-[250px] mt-5">
               <Image
                 source={images.LogoImage}
-                className="z-0 w-full h-[250px]"
+                className="w-full h-[250px]"
+                resizeMode="contain"
               />
             </View>
 
-            <View className="px-3 gap-5 w-full">
+            <View className="px-2 w-full items-center  relative flex-1">
               <Controller
                 control={control}
                 name="username"
                 render={({ field: { onChange, onBlur, value } }) => (
-                  <View className="gap-y-1">
-                    <Text className="font-medium text-lg">Username</Text>
+                  <View className="gap-y-1 w-full">
+                    <Text className="font-inter-regular text-base">Username</Text>
                     <TextInput
                       className="py-3 px-2 rounded-md border"
                       placeholder="Username"
@@ -73,22 +77,25 @@ const LoginScreen = (props: Props) => {
                 control={control}
                 name="password"
                 render={({ field: { onChange, onBlur, value } }) => (
-                  <View className="gap-y-1">
-                    <Text className="font-medium text-lg">Password</Text>
+                  <View className="gap-y-1 w-full">
+                    <Text className="font-inter-regular text-base">Password</Text>
 
-                    <TextInput
-                      style={{
-                        borderWidth: 1,
-                        borderColor: "gray",
-                        marginBottom: 10,
-                        padding: 8,
-                      }}
-                      placeholder="Password"
-                      secureTextEntry
-                      onBlur={onBlur}
-                      onChangeText={onChange}
-                      value={value}
-                    />
+                    <View className="w-full relative">
+                      <TextInput
+                        className="py-3 px-2 rounded-md border w-full"
+                        placeholder="Password"
+                        secureTextEntry={!showPassword}
+                        onBlur={onBlur}
+                        onChangeText={onChange}
+                        value={value}
+                      />
+                      <TouchableOpacity
+                        onPress={() => setShowPassword(!showPassword)}
+                        className="absolute right-1 bottom-1 top-1"
+                      >
+                        {showPassword ? <EyeOff /> : <Eye />}
+                      </TouchableOpacity>
+                    </View>
                     {errors.password && (
                       <Text style={{ color: "red" }}>
                         {errors.password.message}
@@ -108,19 +115,22 @@ const LoginScreen = (props: Props) => {
                 }}
                 className="bg-blue-600 px-5 py-3 rounded-md w-full font-inter-medium flex items-center flex-col"
                 onPress={() => {
+                  alert("Hello submitting");
                   handleSubmit(onSubmit);
                 }}
               >
                 <Text
-                  style={{ fontWeight: "bold" }}
-                  className="text-2xl font-medium text-center text-white"
+                  style={{ fontWeight: "medium" }}
+                  className="text-xl font-inter-regular text-center text-white"
                 >
                   Sign In
                 </Text>
               </TouchableOpacity>
-              <View className="gap-y-3 flex-row gap-x-1">
-                <Text>Je n'ai pas de compte</Text>
-                <Link href={"/(auth)/register"}>Cree</Link>
+              <View className="gap-y-3 flex-row gap-x-1 items-center">
+                <Text className="text-base font-inter-regular">Je n'ai pas de compte</Text>
+                <Link href={"/(auth)/register"}>
+                  <Text className="text-base font-inter-bold">Cree</Text>
+                </Link>
               </View>
             </View>
           </View>
