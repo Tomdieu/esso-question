34e434433f11e554b12a5e49752b31ac3eee5fcb
tomdieu <ivantomdio@gmail.com>
2024-08-30 12:53:20 +0100

    Refactor user store to persist user data using AsyncStorage

--- store/user.ts
+++ store/user.ts
@@ -4,21 +4,21 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { persist } from "zustand/middleware";
 
 type UserStore = {
-    user?: UserType;
-    setUser: (user: UserType) => void;
-    logout: () => void;
+  user?: UserType;
+  setUser: (user: UserType) => void;
+  logout: () => void;
 };
 
 export const useAuth = create<UserStore>()(
-    persist(
-        (set) => ({
-            user: undefined,
-            setUser: (user) => set({ user }),
-            logout: () => set({ user: undefined }),
-        }),
-        {
-            name: "auth",
-            getStorage: () => AsyncStorage,
-        }
-    )
+  persist(
+    (set) => ({
+      user: undefined,
+      setUser: (user) => set({ user }),
+      logout: () => set({ user: undefined }),
+    }),
+    {
+      name: "auth", // unique name for your storage
+      getStorage: () => AsyncStorage, // returns the storage object, not a promise
+    }
+  )
 );
