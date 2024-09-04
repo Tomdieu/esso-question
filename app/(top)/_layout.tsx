0bb59d7eaa25e168136a24dcc27a6bdf0277c995
tomdieu <ivantomdio@gmail.com>
2024-09-05 00:20:34 +0100

    feat : added material top navigator

---
+++ app/(top)/_layout.tsx
@@ -0,0 +1,22 @@
+import {
+  MaterialTopTabNavigationEventMap,
+  MaterialTopTabNavigationOptions,
+  createMaterialTopTabNavigator,
+} from "@react-navigation/material-top-tabs";
+import { ParamListBase, TabNavigationState } from "@react-navigation/native";
+import { withLayoutContext } from "expo-router";
+
+const { Navigator } = createMaterialTopTabNavigator();
+
+export const MaterialTopTabs = withLayoutContext<
+  MaterialTopTabNavigationOptions,
+  typeof Navigator,
+  TabNavigationState<ParamListBase>,
+  MaterialTopTabNavigationEventMap
+>(Navigator);
+
+const Layout = () => {
+  return <MaterialTopTabs />;
+};
+
+export default Layout;
