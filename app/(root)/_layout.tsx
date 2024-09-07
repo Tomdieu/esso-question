0a9cae1986589f986e522c22d508fc0774f71205
tomdieu <ivantomdio@gmail.com>
2024-09-07 23:53:40 +0100

    refactor: Remove unused imports and update screen options in RootLayout component

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -1,4 +1,3 @@
-import { View, Text } from "react-native";
 import React from "react";
 import { Redirect, Stack } from "expo-router";
 import { useAuth } from "@/store/user";
@@ -6,11 +5,11 @@ import { useAuth } from "@/store/user";
 type Props = {};
 
 const RootLayout = (props: Props) => {
-  const { user, logout } = useAuth();
+  const { user } = useAuth();
   if (!user) {
     return <Redirect href={"/(auth)/login"} />;
   }
-  return <Stack />;
+  return <Stack screenOptions={{ headerShown: false }} />;
 };
 
 export default RootLayout;
