33f4a4088d501ffe6edc63fdf78d79d743069b05
tomdieu <ivantomdio@gmail.com>
2024-09-22 09:50:12 +0100

    feat: Add update and delete functions for each array in IdeoGramDatabase
    This commit adds update and delete functions for each array in the IdeoGramDatabase class. These functions allow for updating and deleting specific elements within the arrays of an IdeoGrammeType object. The functions are implemented for the following arrays: problems, solutions, resultats, outilsDevelopement, voiesConsomation, formesCapitatlisation, modelesArchitectural, structurateurs, idees, and modesDePensee. Each function updates the corresponding array element and then updates the IdeoGramStore state with the updated data. Additionally, this commit also adds add functions for each array, which allow for adding new elements to the arrays. These functions are implemented for the arrays: problems, solutions, resultats, outilsDevelopement, voiesConsomation, formesCapitatlisation, modelesArchitectural, structurateurs, idees, and modesDePensee. Each add function appends a new element to the corresponding array and updates the IdeoGramStore state with the updated data.

--- lib/IdeoGramDatabase.ts
+++ lib/IdeoGramDatabase.ts
@@ -121,4 +121,161 @@ export class IdeoGramm {
       id: uuidv4(),
     }));
   }
+
+  // Update functions for each array
+  static updateProblem(ideogram: IdeoGrammeType, index: number, updatedData: Partial<ProblemSchemaType>): void {
+    ideogram.problems[index] = { ...ideogram.problems[index], ...updatedData };
+    useIdeoGramStore.getState().updateIdeogram(ideogram.id, { problems: ideogram.problems });
+  }
+
+  static updateSolution(ideogram: IdeoGrammeType, index: number, updatedData: Partial<SolutionType>): void {
+    ideogram.solutions[index] = { ...ideogram.solutions[index], ...updatedData };
+    useIdeoGramStore.getState().updateIdeogram(ideogram.id, { solutions: ideogram.solutions });
+  }
+
+  static updateResultat(ideogram: IdeoGrammeType, index: number, updatedData: Partial<ResultatType>): void {
+    ideogram.resultats[index] = { ...ideogram.resultats[index], ...updatedData };
+    useIdeoGramStore.getState().updateIdeogram(ideogram.id, { resultats: ideogram.resultats });
+  }
+
+  static updateOutilDevelopement(ideogram: IdeoGrammeType, index: number, updatedData: Partial<OutilDevelopementType>): void {
+    ideogram.outilsDevelopement[index] = { ...ideogram.outilsDevelopement[index], ...updatedData };
+    useIdeoGramStore.getState().updateIdeogram(ideogram.id, { outilsDevelopement: ideogram.outilsDevelopement });
+  }
+
+  static updateVoieConsomation(ideogram: IdeoGrammeType, index: number, updatedData: Partial<VoieConsomationType>): void {
+    ideogram.voiesConsomation[index] = { ...ideogram.voiesConsomation[index], ...updatedData };
+    useIdeoGramStore.getState().updateIdeogram(ideogram.id, { voiesConsomation: ideogram.voiesConsomation });
+  }
+
+  static updateFormeCapitatlisation(ideogram: IdeoGrammeType, index: number, updatedData: Partial<FormeCapitatlisationType>): void {
+    ideogram.formesCapitatlisation[index] = { ...ideogram.formesCapitatlisation[index], ...updatedData };
+    useIdeoGramStore.getState().updateIdeogram(ideogram.id, { formesCapitatlisation: ideogram.formesCapitatlisation });
+  }
+
+  static updateModeleArchitectural(ideogram: IdeoGrammeType, index: number, updatedData: Partial<ModeleArchitecturalType>): void {
+    ideogram.modelesArchitectural[index] = { ...ideogram.modelesArchitectural[index], ...updatedData };
+    useIdeoGramStore.getState().updateIdeogram(ideogram.id, { modelesArchitectural: ideogram.modelesArchitectural });
+  }
+
+  static updateStructurateur(ideogram: IdeoGrammeType, index: number, updatedData: Partial<StructurateurType>): void {
+    ideogram.structurateurs[index] = { ...ideogram.structurateurs[index], ...updatedData };
+    useIdeoGramStore.getState().updateIdeogram(ideogram.id, { structurateurs: ideogram.structurateurs });
+  }
+
+  static updateIdee(ideogram: IdeoGrammeType, index: number, updatedData: Partial<IdeeType>): void {
+    ideogram.idees[index] = { ...ideogram.idees[index], ...updatedData };
+    useIdeoGramStore.getState().updateIdeogram(ideogram.id, { idees: ideogram.idees });
+  }
+
+  static updateModeDePensee(ideogram: IdeoGrammeType, index: number, updatedData: Partial<ModeDePenseeType>): void {
+    ideogram.modesDePensee[index] = { ...ideogram.modesDePensee[index], ...updatedData };
+    useIdeoGramStore.getState().updateIdeogram(ideogram.id, { modesDePensee: ideogram.modesDePensee });
+  }
+
+  // Delete functions for each array
+
+  static deleteProblem(ideogram: IdeoGrammeType, index: number): void {
+    ideogram.problems.splice(index, 1);
+    useIdeoGramStore.getState().updateIdeogram(ideogram.id, { problems: ideogram.problems });
+  }
+
+  static deleteSolution(ideogram: IdeoGrammeType, index: number): void {
+    ideogram.solutions.splice(index, 1);
+    useIdeoGramStore.getState().updateIdeogram(ideogram.id, { solutions: ideogram.solutions });
+  }
+
+  static deleteResultat(ideogram: IdeoGrammeType, index: number): void {
+    ideogram.resultats.splice(index, 1);
+    useIdeoGramStore.getState().updateIdeogram(ideogram.id, { resultats: ideogram.resultats });
+  }
+
+  static deleteOutilDevelopement(ideogram: IdeoGrammeType, index: number): void {
+    ideogram.outilsDevelopement.splice(index, 1);
+    useIdeoGramStore.getState().updateIdeogram(ideogram.id, { outilsDevelopement: ideogram.outilsDevelopement });
+  }
+
+  static deleteVoieConsomation(ideogram: IdeoGrammeType, index: number): void {
+    ideogram.voiesConsomation.splice(index, 1);
+    useIdeoGramStore.getState().updateIdeogram(ideogram.id, { voiesConsomation: ideogram.voiesConsomation });
+  }
+
+  static deleteFormeCapitatlisation(ideogram: IdeoGrammeType, index: number): void {
+    ideogram.formesCapitatlisation.splice(index, 1);
+    useIdeoGramStore.getState().updateIdeogram(ideogram.id, { formesCapitatlisation: ideogram.formesCapitatlisation });
+  }
+
+  static deleteModeleArchitectural(ideogram: IdeoGrammeType, index: number): void {
+    ideogram.modelesArchitectural.splice(index, 1);
+    useIdeoGramStore.getState().updateIdeogram(ideogram.id, { modelesArchitectural: ideogram.modelesArchitectural });
+  }
+
+  static deleteStructurateur(ideogram: IdeoGrammeType, index: number): void {
+    ideogram.structurateurs.splice(index, 1);
+    useIdeoGramStore.getState().updateIdeogram(ideogram.id, { structurateurs: ideogram.structurateurs });
+  }
+
+  static deleteIdee(ideogram: IdeoGrammeType, index: number): void {
+    ideogram.idees.splice(index, 1);
+    useIdeoGramStore.getState().updateIdeogram(ideogram.id, { idees: ideogram.idees });
+  }
+
+  static deleteModeDePensee(ideogram: IdeoGrammeType, index: number): void {
+    ideogram.modesDePensee.splice(index, 1);
+    useIdeoGramStore.getState().updateIdeogram(ideogram.id, { modesDePensee: ideogram.modesDePensee });
+  }
+
+  // Add functions for each array
+
+  static addProblem(ideogram: IdeoGrammeType): void {
+    ideogram.problems.push({ problem: "", id: uuidv4() });
+    useIdeoGramStore.getState().updateIdeogram(ideogram.id, { problems: ideogram.problems });
+  }
+
+  static addSolution(ideogram: IdeoGrammeType): void {
+    ideogram.solutions.push({ solution: "", id: uuidv4() });
+    useIdeoGramStore.getState().updateIdeogram(ideogram.id, { solutions: ideogram.solutions });
+  }
+
+  static addResultat(ideogram: IdeoGrammeType): void {
+    ideogram.resultats.push({ resultat: "", id: uuidv4() });
+    useIdeoGramStore.getState().updateIdeogram(ideogram.id, { resultats: ideogram.resultats });
+  }
+
+  static addOutilDevelopement(ideogram: IdeoGrammeType): void {
+    ideogram.outilsDevelopement.push({ outil_developement: [], id: uuidv4() });
+    useIdeoGramStore.getState().updateIdeogram(ideogram.id, { outilsDevelopement: ideogram.outilsDevelopement });
+  }
+
+  static addVoieConsomation(ideogram: IdeoGrammeType): void {
+    ideogram.voiesConsomation.push({ voie_consomation: [], id: uuidv4() });
+    useIdeoGramStore.getState().updateIdeogram(ideogram.id, { voiesConsomation: ideogram.voiesConsomation });
+  }
+
+  static addFormeCapitatlisation(ideogram: IdeoGrammeType): void {
+    ideogram.formesCapitatlisation.push({ forme_capitatlisation: "", id: uuidv4() });
+    useIdeoGramStore.getState().updateIdeogram(ideogram.id, { formesCapitatlisation: ideogram.formesCapitatlisation });
+  }
+
+  static addModeleArchitectural(ideogram: IdeoGrammeType): void {
+    ideogram.modelesArchitectural.push({ modele_architectural: "", configuration: [], id: uuidv4() });
+    useIdeoGramStore.getState().updateIdeogram(ideogram.id, { modelesArchitectural: ideogram.modelesArchitectural });
+  }
+
+  static addStructurateur(ideogram: IdeoGrammeType): void {
+    ideogram.structurateurs.push({ structurateur: "", natures: [], actions: [], id: uuidv4() });
+    useIdeoGramStore.getState().updateIdeogram(ideogram.id, { structurateurs: ideogram.structurateurs });
+  }
+
+  static addIdee(ideogram: IdeoGrammeType): void {
+    ideogram.idees.push({ idee: "", id: uuidv4() });
+    useIdeoGramStore.getState().updateIdeogram(ideogram.id, { idees: ideogram.idees });
+  }
+
+  static addModeDePensee(ideogram: IdeoGrammeType): void {
+    ideogram.modesDePensee.push({ mode_de_pensee: "", id: uuidv4() });
+    useIdeoGramStore.getState().updateIdeogram(ideogram.id, { modesDePensee: ideogram.modesDePensee });
+  }
+
+  
 }
