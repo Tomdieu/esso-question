644395f629aa10dc7db64413260c2fef9ed85fde
tomdieu <ivantomdio@gmail.com>
2024-09-22 14:49:34 +0100

    updated creative index screen

--- app/(root)/creativite/index.tsx
+++ app/(root)/creativite/index.tsx
@@ -7,176 +7,38 @@ import {
 } from "react-native";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
+import { useIdeoGramStore } from "@/store/ideogram";
+import { router } from "expo-router";
 
 type Props = {};
 
 const CreativiteScreen = (props: Props) => {
+  const { ideograms } = useIdeoGramStore();
   return (
-    <View className="bg-blue-500 flex-1">
+    <View className="bg-slate-50 flex-1">
       <StatusBar style="dark" />
-      <View className="h-56 bg-white m-1"></View>
-
-      <View className="flex-row justify-between py-4 px-2">
-        <TouchableOpacity className="p-2 border border-blue-600 bg-white">
-          <Text className="text-orange-400 font-inter-bold">Maquette</Text>
-        </TouchableOpacity>
-        <TouchableOpacity className="p-2 border border-blue-600 bg-white">
-          <Text className="text-orange-400 font-inter-bold">Traduction</Text>
-        </TouchableOpacity>
-        <TouchableOpacity className="p-2 border border-blue-600 bg-white">
-          <Text className="text-orange-400 font-inter-bold">Prototype</Text>
-        </TouchableOpacity>
-      </View>
-      <View className="justify-center items-center">
-        <Text className="text-2xl font-inter-bold">Creativite</Text>
-      </View>
-      <View className="p-1 ">
-        <ScrollView
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          className="flex-row gap-2 py-2"
-        >
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>PB</Text>
+      {ideograms.length > 0 ? (
+        <View className="flex-1 p-2 gap-y-5">
+          <Text className="text-2xl">Liste des Creativite</Text>
+          <ScrollView>
+            {ideograms.map((ideogram) => (
+              <TouchableOpacity onPress={()=>router.push(`/creativite/${ideogram.id}`)} className="bg-blue-500 p-3 rounded-md">
+                <Text className="text-2xl text-stone-50">{ideogram.project_name}</Text>
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
+        </View>
+      ) : (
+        <View className="flex-1 justify-center px-2 gap-3">
+          <Text className="text-3xl text-center">Pas de Creativite</Text>
+          <TouchableOpacity
+            onPress={() => router.push("/creativite/create")}
+            className="bg-blue-500 p-3 rounded-md justify-center items-center"
+          >
+            <Text className="text-white text-2xl font-inter-bold">Crée</Text>
           </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>Sol</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>Re</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>OD</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>Ve</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>Fe</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>Fab</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>Str</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>Mo</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>Phi</Text>
-          </TouchableOpacity>
-        </ScrollView>
-        <ScrollView
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          className="flex-row gap-2 py-2"
-        >
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>PB</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>Sol</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>Re</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>OD</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>Ve</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>Fe</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>Fab</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>Str</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>Mo</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>Phi</Text>
-          </TouchableOpacity>
-        </ScrollView>
-
-        <ScrollView
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          className="flex-row gap-2 py-2"
-        >
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>PB</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>Sol</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>Re</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>OD</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>Ve</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>Fe</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>Fab</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>Str</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>Mo</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>Phi</Text>
-          </TouchableOpacity>
-        </ScrollView>
-        <ScrollView
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          className="flex-row gap-2 py-2"
-        >
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>PB</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>Sol</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>Re</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>OD</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>Ve</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>Fe</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>Fab</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>Str</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>Mo</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="items-center justify-center p-1.5 border border-blue-500 bg-white w-10 h-10">
-            <Text>Phi</Text>
-          </TouchableOpacity>
-        </ScrollView>
-      </View>
+        </View>
+      )}
     </View>
   );
 };
